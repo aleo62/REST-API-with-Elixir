161b93fe7bd9af1013a658beb4155836a77cb16c
aleo62 <aleiwamoto07@gmail.com>
2025-05-11 12:12:40 -0300

    Get, Delete, Update and Create Posts finished

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,14 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "John", email: "john@example.com"},
+      %{id: 2, name: "Jane", email: "jane@example.com"},
+      %{id: 3, name: "Jim", email: "jim@example.com"},
+    ]
+
+    json(conn, %{users: users})
+  end
 end
